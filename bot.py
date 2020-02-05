985b6e1179dae2f77c9b5d47bb2858f29c059bde
JameJaga <59597475+JameJaga@users.noreply.github.com>
2020-02-05 17:20:40 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,27 @@
+import discord
+import os
 
+TOKEN = os.environ.get("DISCORD_TOKEN")
+
+client = discord.Client()
+num = 0
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+    
+@client.event
+async def on_message(message):
+    guild = message.guild
+    if message.author.bot:
+        return
+  
+  if message.content.startwith(/rankcheckme):
+  
+  if message.content.startwith(/rankcheckyou):
+      if message.author.guild_permissions.administrator:
+          
+      
+
+  
+client.run(TOKEN)
