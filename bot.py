9182d8d75e6cb6c8c5d89b22ab2bb2c8e5ae6cb7
JameJaga <59597475+JameJaga@users.noreply.github.com>
2020-02-08 10:03:27 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,36 +10,45 @@ async def on_ready():
     print('We have logged in as  {0.user}'.format(client))
     
 @client.event
+msg_count = 0
 async def on_message(message):
     guild = message.guild
-    member = message.author
-    msgs = await member.history().flatten()
-    print(msgs)
-    msg_count = len(msgs)
     if message.author.bot:
         return
-    
-    
+    #/rcmeコマンド
     if message.content.startswith('/rcme'):
-        print(msg_count)
-        if(msg_count >= 1000):
-            role = discord.utils.find(lambda r: r.name == 'VIP', member.guild.roles)
-            await member.add_roles(role)
-        elif(msg_count >= 10):
+        member = message.author
+        for channel in guild.text_channels:
+            msgs = await message.channel.history().flatten()
+            msg_count = sum(msg.author == member for msg in msgs)
+        if(msg_count >= 10):
+            role = discord.utils.find(lambda r: r.name == '082', member.guild.roles)
+            await member.remove_roles(role)
             role = discord.utils.find(lambda r: r.name == 'Member', member.guild.roles)
             await member.add_roles(role)
-        
-            
+        if(msg_count >= 1000):
+            role = discord.utils.find(lambda r: r.name == 'Member', member.guild.roles)
+            await member.remove_roles(role)
+            role = discord.utils.find(lambda r: r.name == 'VIP', member.guild.roles)
+            await member.add_roles(role)
+    #/rcyouコマンド
     if message.content.startswith('/rcyou'):
         if message.author.guild_permissions.administrator:
             member = message.mentions[0]
+            for channel in guild.text_channels:
+                msgs = await message.channel.history().flatten()
+                msg_count = sum(msg.author == member for msg in msgs)
             if(msg_count >= 10):
+                role = discord.utils.find(lambda r: r.name == '082', member.guild.roles)
+                await member.remove_roles(role)
                 role = discord.utils.find(lambda r: r.name == 'Member', member.guild.roles)
                 await member.add_roles(role)
             if(msg_count >= 1000):
+                role = discord.utils.find(lambda r: r.name == 'Member', member.guild.roles)
+                await member.remove_roles(role)
                 role = discord.utils.find(lambda r: r.name == 'VIP', member.guild.roles)
                 await member.add_roles(role)
         else:
             embed = discord.Embed(title="AccessDenied",description = 'You do not have permisson to use this command',color=discord.Colour.from_rgb(255, 0, 0))
             await message.channel.send(embed=embed)
-client.run(TOKEN)
+client.run('TOKEN')
